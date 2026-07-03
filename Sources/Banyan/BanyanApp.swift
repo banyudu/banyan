c45ebc5bfa69c6aeb822284d06fad8634ac90fec
banyudu <banyudu@gmail.com>
2026-07-03 13:26:31 +0800

    Activate Banyan when launched from SwiftPM

--- Sources/Banyan/BanyanApp.swift
+++ Sources/Banyan/BanyanApp.swift
@@ -1,7 +1,9 @@
+import AppKit
 import SwiftUI
 
 @main
 struct BanyanApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @StateObject private var store = SessionStore()
 
     var body: some Scene {
@@ -19,5 +21,16 @@ struct BanyanApp: App {
                 .keyboardShortcut("n")
             }
         }
+    }
+}
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
     }
 }
